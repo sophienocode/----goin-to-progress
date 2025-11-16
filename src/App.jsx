0a09913dff9f72efb326364a12f924c2574801ec
sophienocode <sophieprotanki@gmail.com>
2025-11-16 12:18:54 -0300

    add: Outputting dynamic value learning

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
 
   return (
     <>
-      <h1>Hello WebSite</h1>
       < Post />
      
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,27 @@
 /* rafc - rfc  */ 
 
-import React from 'react'
+import React, { useState } from 'react'
+
+/* Outputting dynamic values em React é quando você muda algum valor (state) e o React mostra isso na tela automaticamente. */
+
+const names =  [ 'React fundamentals', 'Fundamentals' ];
 
 function Post()  {
 
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+    const [nome, setNome] = useState("");
+
   return (
     <div>
-        <p>Sophie nocode.</p>
-        <p>React.js is awesome!</p>
+        <p> {chosenName} </p>
+        <p>React.js is {nome} !</p>
+        <input
+        type='text'
+        placeholder='Digite um adjetivo?'
+        onChange={(e) => setNome(e.target.value)}
+        />
+        
     </div>
   )
 }
