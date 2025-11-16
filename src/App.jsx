7a53a8787c424fcfb557d48d3c30b8324fa4874e
sophienocode <sophieprotanki@gmail.com>
2025-11-16 11:42:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Post from "./components/Post";
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello WebSite</h1>
+      < Post />
+     
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,17 @@
+/* rafc - rfc  */ 
+
+import React from 'react'
+
+function Post()  {
+
+  return (
+    <div>
+        <p>Sophie nocode.</p>
+        <p>React.js is awesome!</p>
+    </div>
+  )
+}
+
+export default Post;
+ 
+
